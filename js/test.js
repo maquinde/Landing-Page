6a4c9ae78f934a9d8507249f58ab9787b721e308
Mike <maquinde@outlook.com>
2020-05-23 23:21:18 -0700

    added test solutions for mobile view issue

--- js/test.js
+++ js/test.js
@@ -13,14 +13,67 @@ for(let section of sections){
 
 //IS IN VIEWPORT FUNCTION:
 function isInView(element) {
-    let rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-    );
-};
+
+    var bounding = element.getBoundingClientRect();
+
+    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+
+        console.log('Element is in the viewport!');
+    } else {
+
+        console.log('Element is NOT in the viewport!');
+    }
+
+        // var top = el.offsetTop;
+        // var left = el.offsetLeft;
+        // var width = el.offsetWidth;
+        // var height = el.offsetHeight;
+      
+        // while(el.offsetParent) {
+        //   el = el.offsetParent;
+        //   top += el.offsetTop;
+        //   left += el.offsetLeft;
+        // }
+      
+        // return (
+        //   top < (window.pageYOffset + window.innerHeight) &&
+        //   left < (window.pageXOffset + window.innerWidth) &&
+        //   (top + height) > window.pageYOffset &&
+        //   (left + width) > window.pageXOffset
+        // );
+
+
+//WORKS, BUT HIGHLIGHTS 2 SECTIONS
+    // const rect = element.getBoundingClientRect();
+    // // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+    // const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    // const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    // // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+    // const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    // const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+    // return (vertInView && horInView);
+
+
+//TRY PAGEYOFFSET IN RECT.BOTTOM
+    // let rect = element.getBoundingClientRect();
+    // return (
+    //     rect.top >= 0 &&
+    //     rect.left >= 0 &&
+    //     rect.right <= (window.pageXOffset || document.documentElement.clientWidth) &&
+    //     rect.bottom <= (window.pageYOffset || document.documentElement.clientHeight)
+    // );
+
+//ORIGINAL CODE
+    // let rect = element.getBoundingClientRect();
+    // return (
+    //     rect.top >= 0 &&
+    //     rect.left >= 0 &&
+    //     rect.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+    //     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    // );
+}
 
 //SMOOTH SCROLL FUNCTION:
 function smoothScroll(event){
